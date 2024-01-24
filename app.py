9d7a4961b03910daa97d4c90eb560f80330e9b71
RuichenHe <kylehe970128@gmail.com>
2024-01-24 00:41:39 -0600

    Update the survey page

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/accept')
+@app.route('/survey')
 def accept():
     return render_template('survey.html')
 
 @app.route('/decline')
 def decline():
     return render_template('decline.html')
+
+@app.route('/thanks', methods=['POST', 'GET'])
+def thanks():
+    if request.method == 'POST':
+        return render_template('thanks.html')
+    else:
+        return redirect(url_for('index'))
---
+++ static/js/formScript.js
@@ -0,0 +1,4 @@
+function toggleTextArea(checkbox){
+    var textarea = document.getElementById('conditionalTextarea');
+    textarea.style.display = checkbox.checked ? 'block' : 'none';
+}
